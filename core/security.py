ef1b34424f83a951fb3ff595e39e654e209623cf
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-15 15:55:28 +0300

    Defined the create refresh token function

--- core/security.py
+++ core/security.py
@@ -36,6 +36,28 @@ def create_access_token(_subject: Union[str, Any], _expires_delta: int = None) -
         
     to_encode = {"exp": _expires_delta, "sub": str(_subject)}
     
+    encoded_jwt = jwt.encode(to_encode, settings.JWT_SECRET_KEY, settings.ALGORITHM)
+
+    return encoded_jwt
+
+def create_refresh_token(_subject: Union[str, Any], _expires_delta: int = None) -> str:
+    """Function to create refresh tokens
+
+    Args:
+        _subject (Union[str, Any]): The items to be encoded
+        _expires_delta (int, optional): The expiration time. Defaults to None.
+
+    Returns:
+        str: The access token
+    """
+    if _expires_delta is not None:
+        _expires_delta = datetime.utcnow() + _expires_delta
+    else:
+        _expires_delta = datetime.utcnow() + timedelta(minutes = settings.REFRESH_ACCESS_TOKEN_EXPIRATION_MINUTES)
+        
+    to_encode = {"exp": _expires_delta, "sub": str(_subject)}
+    
     encoded_jwt = jwt.encode(to_encode, settings.JWT_REFRESH_SECRET_KEY, settings.ALGORITHM)
 
     return encoded_jwt
+
