2fdeb762f1e0e5fbb862ede04a73f27b7d9588e9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-15 11:54:38 +0300

    Wrote the Settings class and instantiated it

--- core/config.py
+++ core/config.py
@@ -1,2 +1,35 @@
 """The configuration file for the app"""
-from pydantic import BaseSettings
+from typing import List
+from pydantic import AnyHttpUrl, BaseSettings
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+
+JWT_SECRET_KEY_ENV = os.environ.get("JWT_SECRET_KEY")
+JWT_REFRESH_SECRET_KEY_ENV = os.environ.get("JWT_REFRESH_SECRET_KEY")
+MONGODB_URL_ENV = os.environ.get("MONGODB_URL")
+
+class Settings(BaseSettings):
+    """The settings for the application
+
+    Args:
+        BaseSettings (Pydantic): The parent class for Settings
+    """
+    JWT_SECRET_KEY: str = JWT_SECRET_KEY_ENV
+    JWT_REFRESH_SECRET_KEY: str = JWT_REFRESH_SECRET_KEY_ENV
+    ALGORITHM = 'HS256'
+    ACCESS_TOKEN_EXPIRATION_MINUTES: int = 15
+    REFRESH_ACCESS_TOKEN_EXPIRATION_MINUTES: int = 60*24*7   # 7 days
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+    
+    # Database
+    MONGODB_CONN_STRING = MONGODB_URL_ENV
+    
+    class Config: 
+        """The configuration for the settings class"""
+        case_sensitive = True
+        
+
+settings = Settings()
