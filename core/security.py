cb4cd54145f57b67f0adee37459b947f19a0c8dc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-15 16:02:32 +0300

    Took care of a single spacing issue

--- core/security.py
+++ core/security.py
@@ -62,7 +62,6 @@ def create_refresh_token(_subject: Union[str, Any], _expires_delta: int = None) 
 
     to_encode = {"exp": _expires_delta, "sub": str(_subject)}
 
-    encoded_jwt = jwt.encode(
-        to_encode, settings.JWT_REFRESH_SECRET_KEY, settings.ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, settings.JWT_REFRESH_SECRET_KEY, settings.ALGORITHM)
 
     return encoded_jwt
