926046e70aa87d98585e8143bd2505e07ef2152c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-15 12:09:59 +0300

    Drfined the user model

--- models/user_model.py
+++ models/user_model.py
@@ -1,2 +1,24 @@
 """The User Models"""
+from typing import Optional
+from uuid import UUID, uuid4
 from beanie import Document, Indexed
+from pydantic import EmailStr, Field
+
+
+class User(Document):
+    """The model for the User data
+
+    Args:
+        Document (beanie.Document): The parent for the User model
+    """
+    user_id: UUID = Field(default_factory=uuid4)
+    user_name = Indexed(str, unique=True)
+    email: Indexed(EmailStr, unique=True)
+    hashed_password: str
+    first_name: Optional[str]
+    last_name: Optional[str]
+    disabled: Optional[bool]
+    
+    class Settings:
+        """The name of the collection"""
+        name = "users"
