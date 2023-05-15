40fc66ca84f25d510b05aa0ce7fb2b8e2a357925
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-15 13:20:52 +0300

    Added a custom status code for the create user endpoint

--- routes/user_router.py
+++ routes/user_router.py
@@ -1,5 +1,5 @@
 """The router file for the user"""
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 
 from schemas.user_schema import UserAuth
 
@@ -9,6 +9,6 @@ from services.user_services import UserService
 user_router = APIRouter(prefix="/user", tags=["User"])
 
 
-@user_router.post("/create", name="Create User", description="An endpoint for creating new users for the app")
+@user_router.post("/create", name="Create User", description="An endpoint for creating new users for the app", status_code=status.HTTP_201_CREATED)
 async def create_user(_data: UserAuth):
     await UserService.create_user(_data)
