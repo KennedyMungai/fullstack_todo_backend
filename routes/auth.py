e032a1ffb02bffd4365fe976f57fe56f69a31ff8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-15 15:03:54 +0300

    Raised a 403 if the user credentials are incorrect

--- routes/auth.py
+++ routes/auth.py
@@ -1,14 +1,21 @@
 """The file that holds the auth endpoints for the users"""
 from typing import Any
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from fastapi.security import OAuth2PasswordRequestForm
 
 from services.user_services import UserService
+from fastapi import HTTPException
 
 auth_router = APIRouter(prefix="/auth", tags=["Auth"])
 
 
 @auth_router.post("/login")
 async def login(_form_data: OAuth2PasswordRequestForm = Depends()) -> Any:
-    user = await UserService.authenticate(_form_data.email, _form_data.password)
+    _user = await UserService.authenticate(_form_data.email, _form_data.password)
+    
+    if not _user:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, 
+            detail="Incorrect email or password"
+            )
