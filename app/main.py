1ed4bf47fc6d5064fe3642c1db27346663690749
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-15 12:00:35 +0300

    Wrote the logic for the startup event

--- app/main.py
+++ app/main.py
@@ -2,12 +2,22 @@
 from fastapi import FastAPI
 from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
+from core.config import settings
 
 
 app = FastAPI(
     title="Todo Backend",
     description="The backend of a comprehensive todo application"
 )
+
+
+@app.on_event("startup")
+async def startup_event():
+    """Logic to run at app startup"""
+    print("Connecting to the database")
+    db_client = AsyncIOMotorClient(settings.MONGODB_CONN_STRING).todolist
+    await init_beanie(db_client, document_models=[])
+    print("Connected to the database")
 
 
 @app.get("/", name="root", description="The root endpoint", tags=["Root"])
