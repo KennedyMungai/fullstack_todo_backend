f42641e5e62f5d9a5043a8ea5e6bc1dee30a0f14
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-15 13:12:42 +0300

    Wrote functions to create a password hash and to verify a hashed password

--- core/security.py
+++ core/security.py
@@ -3,3 +3,12 @@ from passlib.context import CryptContext
 
 
 password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def hash_password(_password: str) -> str:
+    """Hash the password"""
+    return password_context.hash(_password)
+
+def verify_password(_password: str, hashed_password: str) -> bool:
+    """Verify the password"""
+    return password_context.verify(_password, hashed_password)
