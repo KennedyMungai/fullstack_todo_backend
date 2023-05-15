a8052f0c4d810f50b1588156298ecc4ccaee473c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-15 14:49:22 +0300

    Made _login a dependency

--- routes/auth.py
+++ routes/auth.py
@@ -1,11 +1,12 @@
 """The file that holds the auth endpoints for the users"""
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
+from typing import Any
 
 
 auth_router = APIRouter(prefix="/auth", tags=["Auth"])
 
 
 @auth_router.post("/login")
-async def login(_login: OAuth2PasswordRequestForm):
+async def login(_login: OAuth2PasswordRequestForm = Depends()) -> Any:
     pass
