7de9bc6123078dd2568ef50a87d836f3e0933bf2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-15 13:00:52 +0300

    Set the data type for data to be the UserAuth schema

--- routes/user_router.py
+++ routes/user_router.py
@@ -1,10 +1,12 @@
 """The router file for the user"""
 from fastapi import APIRouter
 
+from schemas.user_schema import UserAuth
+
 
 user_router = APIRouter(prefix="/user", tags=["User"])
 
 
 @user_router.post("/create", name="Create User", description="An endpoint for creating new users for the app")
-async def create_user(data):
+async def create_user(data: UserAuth):
     return {"message": "Create User endpoint"}
