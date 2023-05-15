02919430d58a845a386911d4272da6c5d5ea0502
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-15 14:46:26 +0300

    Wrote the skeleton for the user login endpoint

--- routes/auth.py
+++ routes/auth.py
@@ -1,1 +1,10 @@
 """The file that holds the auth endpoints for the users"""
+from fastapi import APIRouter
+
+
+auth_router = APIRouter(prefix="/auth", tags=["Auth"])
+
+
+@auth_router.post("/login")
+async def login():
+    pass
