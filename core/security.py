98980ac20fec807ec2781afc0baadfd675e1e867
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-15 15:55:50 +0300

    Formatted the document

--- core/security.py
+++ core/security.py
@@ -19,6 +19,7 @@ def verify_password(_password: str, _hashed_password: str) -> bool:
     """Verify the password"""
     return password_context.verify(_password, _hashed_password)
 
+
 def create_access_token(_subject: Union[str, Any], _expires_delta: int = None) -> str:
     """Function to create access tokens
 
@@ -32,13 +33,16 @@ def create_access_token(_subject: Union[str, Any], _expires_delta: int = None) -
     if _expires_delta is not None:
         _expires_delta = datetime.utcnow() + _expires_delta
     else:
-        _expires_delta = datetime.utcnow() + timedelta(minutes = settings.ACCESS_TOKEN_EXPIRATION_MINUTES)
-        
+        _expires_delta = datetime.utcnow(
+        ) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRATION_MINUTES)
+
     to_encode = {"exp": _expires_delta, "sub": str(_subject)}
-    
-    encoded_jwt = jwt.encode(to_encode, settings.JWT_SECRET_KEY, settings.ALGORITHM)
+
+    encoded_jwt = jwt.encode(
+        to_encode, settings.JWT_SECRET_KEY, settings.ALGORITHM)
 
     return encoded_jwt
+
 
 def create_refresh_token(_subject: Union[str, Any], _expires_delta: int = None) -> str:
     """Function to create refresh tokens
@@ -53,11 +57,12 @@ def create_refresh_token(_subject: Union[str, Any], _expires_delta: int = None) 
     if _expires_delta is not None:
         _expires_delta = datetime.utcnow() + _expires_delta
     else:
-        _expires_delta = datetime.utcnow() + timedelta(minutes = settings.REFRESH_ACCESS_TOKEN_EXPIRATION_MINUTES)
-        
+        _expires_delta = datetime.utcnow(
+        ) + timedelta(minutes=settings.REFRESH_ACCESS_TOKEN_EXPIRATION_MINUTES)
+
     to_encode = {"exp": _expires_delta, "sub": str(_subject)}
-    
-    encoded_jwt = jwt.encode(to_encode, settings.JWT_REFRESH_SECRET_KEY, settings.ALGORITHM)
+
+    encoded_jwt = jwt.encode(
+        to_encode, settings.JWT_REFRESH_SECRET_KEY, settings.ALGORITHM)
 
     return encoded_jwt
-
