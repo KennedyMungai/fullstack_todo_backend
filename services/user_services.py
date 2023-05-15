f65d7070f01a2ca510f9c0ca368e47fac16a4cf2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-15 13:06:51 +0300

    Created the _user_in instance if User

--- services/user_services.py
+++ services/user_services.py
@@ -1,1 +1,13 @@
 """The file to hold the logic handling the services for the User"""
+from schemas.user_schema import UserAuth
+from models.user_model import User
+
+
+class UserService:
+    @staticmethod
+    async def create_user(_user: UserAuth):
+        _user_in = User(
+            email=_user.email,
+           username= _user.username,
+           hashed_password= _user.password
+        )
