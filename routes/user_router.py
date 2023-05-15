b2cb73d786e66fc2ae1d9e85ad5b45157093604e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-15 12:51:46 +0300

    Wrote a test route for the user router

--- routes/user_router.py
+++ routes/user_router.py
@@ -3,3 +3,9 @@ from fastapi import APIRouter
 
 
 user_router = APIRouter(prefix="/user", tags=["User"])
+
+
+@user_router.get("/", name="User Test Route", description="The route to test the user router")
+async def user_test() -> dict[str, str]:
+    """The test route for the user router"""
+    return {"message": "User Test Route"}
