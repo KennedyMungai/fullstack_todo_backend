09e7665f8ffb2489272eddd91e642dba9eaa1033
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-15 11:21:31 +0300

    Wrote the base endpoint for the application

--- app/main.py
+++ app/main.py
@@ -6,3 +6,8 @@ app = FastAPI(
     title="Todo Backend", 
     description="The backend of a comprehensive todo application"
     )
+
+
+@app.get("/", name="root", description="The root endpoint", tags=["Root"])
+async def root() -> dict[str, str]:
+    return {"message": "The API works"}
