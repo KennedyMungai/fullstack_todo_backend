f729f38602833d57bb828d57d5002c7194ebdf4a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-15 11:19:47 +0300

    Made an instance of the FastApi class

---
+++ app/main.py
@@ -0,0 +1,8 @@
+"""The main file for the project"""
+from fastapi import FastAPI
+
+
+app = FastAPI(
+    title="Todo Backend", 
+    description="The backend of a comprehensive todo application"
+    )
