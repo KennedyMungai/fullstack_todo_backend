8677d91121b450ce8ac031d9897b81fac9622fc7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-15 12:55:29 +0300

    Wrote the skeleton for the user create endpoint

--- routes/user_router.py
+++ routes/user_router.py
@@ -5,7 +5,6 @@ from fastapi import APIRouter
 user_router = APIRouter(prefix="/user", tags=["User"])
 
 
-@user_router.get("/", name="User Test Route", description="The route to test the user router")
-async def user_test() -> dict[str, str]:
-    """The test route for the user router"""
-    return {"message": "User Test Route"}
+@user_router.post("/create", name="Create User", description="An endpoint for creating new users for the app")
+async def create_user(data):
+    return {"message": "Create User endpoint"}
