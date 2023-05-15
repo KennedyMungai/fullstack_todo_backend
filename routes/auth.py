93525448878270c625b2d70d7a1fc05da60d516e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-15 14:47:13 +0300

    Imported the OAuth2PasswordRequestForm

--- routes/auth.py
+++ routes/auth.py
@@ -1,5 +1,6 @@
 """The file that holds the auth endpoints for the users"""
 from fastapi import APIRouter
+from fastapi.security import OAuth2PasswordRequestForm
 
 
 auth_router = APIRouter(prefix="/auth", tags=["Auth"])
