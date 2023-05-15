6a27bf31a49f4072b52eec84c8d5e1bcd4707f52
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-15 13:00:00 +0300

    Added a docstring for the UserAuth schema

--- schemas/user_schema.py
+++ schemas/user_schema.py
@@ -3,6 +3,11 @@ from pydantic import BaseModel, EmailStr, Field
 
 
 class UserAuth(BaseModel):
+    """The User Auth schema
+
+    Args:
+        BaseModel (Pydantic): The parent class for the UserAuth schema
+    """
     email: EmailStr = Field(..., description="The user email")
     username: str = Field(..., min_length=3, max_length=400, description="The user username")
     password: str = Field(..., min_length=5, max_length=24, description="The user password")
