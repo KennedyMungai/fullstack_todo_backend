5c6879bb77cf2baac999853b9784cd514496f7a5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-15 15:02:13 +0300

    Reorganized the imports for the python script

--- routes/auth.py
+++ routes/auth.py
@@ -1,9 +1,10 @@
 """The file that holds the auth endpoints for the users"""
+from typing import Any
+
 from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
-from typing import Any
-from services.user_services import UserService
 
+from services.user_services import UserService
 
 auth_router = APIRouter(prefix="/auth", tags=["Auth"])
 
