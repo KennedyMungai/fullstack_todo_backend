d4b9dfbc4b17862f17f3ef906aa4feb4946423bc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-16 15:43:00 +0300

    Defined the skeleton for the TodoService class and the list_todos static function and imported the models for User and Todo

--- services/todo_service.py
+++ services/todo_service.py
@@ -1,1 +1,8 @@
 """The service file for Todos"""
+from models.todo_model import Todo
+from models.user_model import User
+
+class TodoService:
+    @staticmethod
+    async def list_todos(_user: User):
+        pass
