db50556d1da9ca44dd74832255b2f076557e1bff
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-16 15:31:10 +0300

    Created the TodoUpdate schema for when a Todo is being updated

--- schemas/todo_schema.py
+++ schemas/todo_schema.py
@@ -5,5 +5,11 @@ from typing import Optional
 
 class TodoCreate(BaseModel):
     title: str = Field(..., title="Title", max_length=55, min_length=1, description="Title of the todo")
-    description: str = Field(..., title="Description", max_length=10000, min_length=1, description="Description of the todo")
+    description: str = Field(..., title="Description", max_length=1000, min_length=1, description="Description of the todo")
+    status: Optional[bool] = False
+    
+    
+class TodoUpdate(BaseModel):
+    title: Optional[str] = Field(None, title="Title", max_length=55, min_length=1, description="Title of the todo")
+    description: Optional[str] = Field(None, title="Description", max_length=1000, min_length=1, description="Description of the todo")
     status: Optional[bool] = False
