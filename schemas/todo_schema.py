9fc01c705da5813ef79d6748d6f4fd30e865d0b9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-16 15:29:27 +0300

    Created the TodoCreate schema for when a person is creating a Todo

--- schemas/todo_schema.py
+++ schemas/todo_schema.py
@@ -1,2 +1,9 @@
 """Created a Todo schema file"""
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Optional
+
+
+class TodoCreate(BaseModel):
+    title: str = Field(..., title="Title", max_length=55, min_length=1, description="Title of the todo")
+    description: str = Field(..., title="Description", max_length=10000, min_length=1, description="Description of the todo")
+    status: Optional[bool] = False
