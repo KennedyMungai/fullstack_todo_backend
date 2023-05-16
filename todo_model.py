63c6b01c709379b62cd5a0b818b72a3b945606ad
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-16 14:48:31 +0300

    Created the Todo model

--- todo_model.py
+++ todo_model.py
@@ -1,1 +1,31 @@
 """The file to hold the models for the Todos"""
+from datetime import datetime
+from uuid import UUID, uuid4
+
+from beanie import Document, Indexed, Link
+from pydantic import Field
+
+from models.user_model import User
+
+
+class Todo(Document):
+    todo_id: UUID = Field(default_factory=uuid4, unique=True)
+    status: bool = False
+    title: Indexed(str)
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    updated_at: datetime = Field(default_factory=datetime.utcnow)
+    owner: Link[User]
+    
+    def __repr__(self) -> str:
+        return f"Todo: {self.title}"
+    
+    def __str__(self) -> str:
+        return self.title
+    
+    def __hash__(self) -> int:
+        return hash(self.title)
+    
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Todo):
+            return False
+        return self.title == other.title
