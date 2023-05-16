80f76cf0b3058214e285c02b52bdcdcb3a5aec15
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-16 15:57:05 +0300

    Defined the skeleton of the create_todo static method

--- services/todo_service.py
+++ services/todo_service.py
@@ -3,9 +3,15 @@ from models.todo_model import Todo
 from models.user_model import User
 from typing import List
 
+from schemas.todo_schema import TodoCreate
+
 class TodoService:
     @staticmethod
     async def list_todos(_user: User) -> List[Todo]:
         _todos = await Todo.find(Todo.owner.id == _user.user_id).to_list()
         return _todos
+    
+    @staticmethod
+    async def create_todo(_todo: TodoCreate, _current_user: User) -> Todo:
+        pass
         
