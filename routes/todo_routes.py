e8de034b8c2b51a37dbd663ccef0f05fd60ffe97
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-16 16:11:12 +0300

    Leveraged the yet to be created retrieve_todo TodoService static method to retrive a single todo from the database

--- routes/todo_routes.py
+++ routes/todo_routes.py
@@ -42,3 +42,8 @@ async def get_all_todos(_current_user: User = Depends(get_current_user)):
     )
 async def create_todo_endpoint(_todo: TodoCreate, _current_user: User = Depends(get_current_user)):
     return await TodoService.create_todo(_todo, _current_user)
+
+
+@todo_router.get("/{_todo_id}", name="Retrieve Todo", description="An endpoint to retrieve a Todo", response_model=TodoOut)
+async def retrieve_one_todo_endpoint(_todo_id: int, _current_user: User = Depends(get_current_user)):
+    return await TodoService.retrieve_todo(_todo_id, _current_user)
