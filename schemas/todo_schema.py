0f561b21212ac130cc2b0496f61544f9aeb003c4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-16 15:32:39 +0300

    Created TodoOut which is a schema for when Todos are being retrieved from the database

--- schemas/todo_schema.py
+++ schemas/todo_schema.py
@@ -1,4 +1,6 @@
 """Created a Todo schema file"""
+from datetime import datetime
+from uuid import UUID
 from pydantic import BaseModel, Field
 from typing import Optional
 
@@ -13,3 +15,11 @@ class TodoUpdate(BaseModel):
     title: Optional[str] = Field(None, title="Title", max_length=55, min_length=1, description="Title of the todo")
     description: Optional[str] = Field(None, title="Description", max_length=1000, min_length=1, description="Description of the todo")
     status: Optional[bool] = False
+
+
+class TodoOut(BaseModel):
+    todo_id: UUID
+    status: bool
+    title: str
+    description: str
+    created_at: datetime
