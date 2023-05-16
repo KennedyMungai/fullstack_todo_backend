2ffa4b3be9df30867ae07e72dcecfb70051e013d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-16 11:49:34 +0300

    Wrote some of the code for the try block for the get_current_user function

--- dependencies/user_dependencies.py
+++ dependencies/user_dependencies.py
@@ -1,5 +1,15 @@
 """The file which holds the user dependencies"""
+from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
+from models.user_model import User
+from jose import jwt
+from core.config import settings
 
 
 reusable_oauth = OAuth2PasswordBearer(tokenUrl='/auth/login', scheme_name='JWT')
+
+
+async def get_current_user(token: str = Depends(reusable_oauth)) -> User:
+    try: 
+        payload = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=[settings.ALGORITHM])
+        token_data = Token()
