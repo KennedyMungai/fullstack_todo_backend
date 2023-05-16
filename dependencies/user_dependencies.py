903e1b0620f27c9f778bc6396a6838dd74effbe0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-16 12:03:49 +0300

    Raised a 401 is the token has expired

--- dependencies/user_dependencies.py
+++ dependencies/user_dependencies.py
@@ -1,9 +1,12 @@
 """The file which holds the user dependencies"""
-from fastapi import Depends
+from datetime import datetime
+from fastapi import Depends, status
 from fastapi.security import OAuth2PasswordBearer
 from models.user_model import User
 from jose import jwt
 from core.config import settings
+from schemas.auth_schema import TokenPayload
+from fastapi import HTTPException
 
 
 reusable_oauth = OAuth2PasswordBearer(tokenUrl='/auth/login', scheme_name='JWT')
@@ -12,4 +15,10 @@ reusable_oauth = OAuth2PasswordBearer(tokenUrl='/auth/login', scheme_name='JWT')
 async def get_current_user(token: str = Depends(reusable_oauth)) -> User:
     try: 
         payload = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=[settings.ALGORITHM])
-        token_data = Token()
+        token_data = TokenPayload(**payload)
+        
+        if datetime.fromtimestamp(token_data.exp) < datetime.now():
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, 
+                detail='Token expired'
+                )
