fff9411599f8c9c78c3853ce760ecd859e02a59b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-16 13:03:12 +0300

    Spaced out the decode function call

--- routes/auth_route.py
+++ routes/auth_route.py
@@ -91,8 +91,11 @@ async def refresh_access_token(_refresh_token: str = Body(...)) -> TokenSchema:
         TokenSchema: The new access token
     """
     try:
-        payload = jwt.decode(_refresh_token, settings.JWT_REFRESH_SECRET_KEY,
-                             algorithms=[settings.ALGORITHM])
+        payload = jwt.decode(
+            _refresh_token, 
+            settings.JWT_REFRESH_SECRET_KEY,
+            algorithms=[settings.ALGORITHM]
+            )
         token_data = TokenPayload(**payload)
 
         if datetime.fromtimestamp(token_data.exp) < datetime.now():
