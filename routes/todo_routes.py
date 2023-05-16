92fa029a6a2791b0c801498ac739491aa88f5405
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-16 14:37:55 +0300

    Formatte dthe document

--- routes/todo_routes.py
+++ routes/todo_routes.py
@@ -6,11 +6,11 @@ todo_router = APIRouter(prefix="/todo", tags=["Todo"])
 
 
 @todo_router.get(
-    "/", 
-    name="Test Todo router", 
-    description="An endpoint to test the Todo router",  
+    "/",
+    name="Test Todo router",
+    description="An endpoint to test the Todo router",
     response_model=dict
-    )
+)
 async def test_todo_router():
     """Test Todo router"""
     return {"Todo Router": "Works"}
