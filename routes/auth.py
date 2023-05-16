f0bd9297a0e9760159e2264ae828b90aed5d7ce9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-16 11:58:58 +0300

    Added a response model for the login route

--- routes/auth.py
+++ routes/auth.py
@@ -4,6 +4,7 @@ from typing import Any
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from core.security import create_access_token, create_refresh_token
+from schemas.auth_schema import TokenSchema
 
 from services.user_services import UserService
 
@@ -14,7 +15,8 @@ auth_router = APIRouter(prefix="/auth", tags=["Auth"])
     "/login",
     name="Login",
     status_code=status.HTTP_200_OK,
-    description="Create Access and Refresh tokens"
+    description="Create Access and Refresh tokens",
+    response_model=TokenSchema
 )
 async def login(_form_data: OAuth2PasswordRequestForm = Depends()) -> Any:
     """The login endpoint
