371bf806dd0ba86bc638351d358d3afda70c4fa5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-16 11:44:50 +0300

    Created the OAuth2PasswordBearere instance called resuable_auth

--- dependencies/user_dependencies.py
+++ dependencies/user_dependencies.py
@@ -1,2 +1,5 @@
 """The file which holds the user dependencies"""
 from fastapi.security import OAuth2PasswordBearer
+
+
+reusable_oauth = OAuth2PasswordBearer(tokenUrl='/auth/login', scheme_name='JWT')
