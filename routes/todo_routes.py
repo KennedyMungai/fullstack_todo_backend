02547aead6890cd10fc38bd2f4cc6ae38736352e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-16 14:37:16 +0300

    Defined a test endpoint for the todo_router

--- routes/todo_routes.py
+++ routes/todo_routes.py
@@ -3,3 +3,14 @@ from fastapi import APIRouter, Depends
 
 
 todo_router = APIRouter(prefix="/todo", tags=["Todo"])
+
+
+@todo_router.get(
+    "/", 
+    name="Test Todo router", 
+    description="An endpoint to test the Todo router",  
+    response_model=dict
+    )
+async def test_todo_router():
+    """Test Todo router"""
+    return {"Todo Router": "Works"}
